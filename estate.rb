26f650ac73ec7db6c0c6e28a1ed83fb6ae289e96
Zhangzhe Shen <zzshen@thoughtworks.com>
2016-11-15 20:19:58 +0800

    build estate

--- estate.rb
+++ estate.rb
@@ -1,15 +1,27 @@
-
 class Estate
-
-  attr_accessor :position, :price, :owner
+  MAX_LEVEL = 3
+  attr_accessor :position, :price, :owner, :level
 
   def initialize(position, price)
     @position = position
     @price = price
+    @level = 0
   end
 
   def visit_by(player)
-    player.execute BuyLandCommand.new self
+    if @owner.nil?
+      player.execute BuyLandCommand.new self
+    elsif @owner.equal? player
+      player.execute BuildLandCommand.new self
+    end
+  end
+
+  def max_level?
+    @level.equal? MAX_LEVEL
+  end
+
+  def build
+    @level += 1
   end
 
 end
@@ -36,3 +48,23 @@ class BuyLandCommand
 
 end
 
+class BuildLandCommand
+  def initialize(estate)
+    @estate = estate
+  end
+
+  def execute(player)
+    player.status = Player::Status::WAIT_FOR_RESPONSE
+  end
+
+  def respond(player, response)
+    case response.upcase
+      when 'N'
+      when 'Y'
+        if !@estate.max_level? && player.reduce_money(@estate.price)
+          @estate.build
+        end
+    end
+    player.status = Player::Status::TURN_END
+  end
+end
--- spec/roll_command_spec.rb
+++ spec/roll_command_spec.rb
@@ -30,6 +30,17 @@ describe RollCommand do
       expect(@player.status).to eq(Player::Status::WAIT_FOR_RESPONSE)
     end
 
+    it 'should move player to empty estate' do
+      estate = Estate.new 0, ESTATE_PRICE
+      estate.owner = @player
+      allow(@map).to receive(:place_at).with(1) { estate }
+
+      @player.execute @command
+
+      expect(@player.location).to eq(1)
+      expect(@player.status).to eq(Player::Status::WAIT_FOR_RESPONSE)
+    end
+
   end
 
   describe '#respond' do
@@ -70,6 +81,59 @@ describe RollCommand do
       end
     end
 
+    describe 'player visit own estate' do
+
+      before(:each) do
+        @estate = Estate.new 0, ESTATE_PRICE
+        @estate.owner = @player
+        allow(@map).to receive(:place_at).with(1) { @estate }
+
+        @player.execute @command
+
+        expect(@player.location).to eq(1)
+        expect(@player.status).to eq(Player::Status::WAIT_FOR_RESPONSE)
+      end
+
+      it 'should turn end when player say no' do
+        @player.respond 'n'
+
+        expect(@player.status).to eq(Player::Status::TURN_END)
+        expect(@player.money).to eq(INITIAL_BALANCE)
+      end
+
+      it 'should success to build estate turn end when player say yes with enough money' do
+        @player.respond 'y'
+
+        expect(@player.status).to eq(Player::Status::TURN_END)
+        expect(@player.money).to eq(INITIAL_BALANCE - ESTATE_PRICE)
+        expect(@estate.owner).to eq(@player)
+        expect(@estate.level).to eq(1)
+      end
+
+      it 'should failed to build estate turn end when player say yes without enough money' do
+        @player.money = 0
+        @player.respond 'y'
+
+        expect(@player.status).to eq(Player::Status::TURN_END)
+        expect(@player.money).to eq(0)
+        expect(@estate.owner).to eq(@player)
+        expect(@estate.level).to eq(0)
+      end
+
+      it 'should failed to build estate turn end when estate max level' do
+        @estate.level = 3
+        @player.respond 'y'
+
+        expect(@player.status).to eq(Player::Status::TURN_END)
+        expect(@player.money).to eq(INITIAL_BALANCE)
+        expect(@estate.owner).to eq(@player)
+        expect(@estate.level).to eq(3)
+      end
+
+      after(:each) do
+        expect(@player.status).to eq(Player::Status::TURN_END)
+      end
+    end
   end
 
 end
